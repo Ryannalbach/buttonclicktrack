8cca1c6b2967a3c7a229692c91808099f476ff95
Ryan Nalbach <103783337+Ryannalbach@users.noreply.github.com>
2022-10-19 16:18:07 -0700

    added function for button, need to add html

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const App = () => {
+  let a = [1, 2, 3, 4];
+  const handler = e => alert(`button ${e.target.getAttribute("index")}`);
+  let list = a.map((item, index) => {
+    return <myButton index={index} onClick={handler} key={index}></myButton>;
+  });
+  return <div>{list}</div>;
+};
+const myButton = ({onClick, index}) => {
+  let { Button } = ReactBootstrap;
+  return (
+    <button index={index} onClick={onClick}>
+      Click Me
+    </button>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +31,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
